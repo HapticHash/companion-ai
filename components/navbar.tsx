fb314582c19dfbea0f4df36044491fe05cabb0b4
Harshit S <harshitks2203@gmail.com>
2023-08-10 20:42:05 -0500

    added navbar

---
+++ components/navbar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { Poppins } from "next/font/google";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { Menu, Sparkles } from "lucide-react";
+import { ModeToggle } from "./mode-toggle";
+
+const font = Poppins({ weight: "600", subsets: ["latin"] });
+
+interface NavbarProps {
+  isPro: boolean;
+}
+
+export const Navbar = () => {
+  return (
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary">
+      <div className="flex items-center">
+        <Menu className="block md:hidden" />
+        <Link href="/">
+          <h1
+            className={cn(
+              "hidden md:block text-xl md:text-3xl font-bold text-primary",
+              font.className
+            )}
+          >
+            companion.ai
+          </h1>
+        </Link>
+      </div>
+      <div className="flex items-center gap-x-3">
+        <Button variant="premium" size="sm">
+          Upgrade
+          <Sparkles className="h-4 w-4 fill-white text-white ml-2" />
+        </Button>
+        <ModeToggle />
+        <UserButton />
+      </div>
+    </div>
+  );
+};
