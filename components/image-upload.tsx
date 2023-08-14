93739d292ceb2b0c081fab876c8c1c51c3e5bdf4
Harshit S <harshitks2203@gmail.com>
2023-08-13 21:18:06 -0500

    updated companion form and added image upload

---
+++ components/image-upload.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { CldUploadButton } from "next-cloudinary";
+import Image from "next/image";
+
+interface ImageUploadProps {
+  value: string;
+  onChange: (src: string) => void;
+  disabled?: boolean;
+}
+
+const ImageUpload = ({ value, onChange, disabled }: ImageUploadProps) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="space-y-4 w-full flex flex-col justify-center items-center">
+      <CldUploadButton
+        options={{ maxFiles: 1 }}
+        onUpload={(result: any) => onChange(result.info.secure_url)}
+        uploadPreset="fvevdqqd"
+      >
+        <div className="p-4 border-4 border-dashedborder-primary/10 rounded-lg hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center">
+          <div className="relative h-40 w-40">
+            <Image
+              fill
+              alt="Upload"
+              src={value || "/placeholder.svg"}
+              className="rounded-lg object-cover"
+            />
+          </div>
+        </div>
+      </CldUploadButton>
+    </div>
+  );
+};
+
+export default ImageUpload;
