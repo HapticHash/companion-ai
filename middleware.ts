4dd352d188cf0228a93150855b45e6132dbbe673
Harshit S <harshitks2203@gmail.com>
2023-08-19 19:56:28 -0500

    added stripe API

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,9 @@ import { NextResponse } from "next/server";
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
-export default authMiddleware({});
+export default authMiddleware({
+  publicRoutes: ["api/webhook"],
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
